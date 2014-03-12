18a78c6356e7576d76933467a9d922c856f19d07
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-03-12 17:01:49 -0400

    Initial bare bones rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopAnnarborteesCom::Application.config.secret_key_base = 'e6f0bb07a86c0d24d30d7222b5f5899594ada634c7813c5458b4cf380dac271d243057027176bbca14f78a23a296ce22d5ba2b1e1b912ae7f6748e9c3b5e4bcf'
