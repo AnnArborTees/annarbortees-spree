6b992d2be008ebfa98f042d982722d07bf5d5a90
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-05-30 12:40:51 -0400

    Using a better google font, eliminating cloudfront from the asset equation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ ShopAnnarborteesCom::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "http://assets%d.annarbortees.com"
+  #config.action_controller.asset_host = "http://assets%d.annarbortees.com"
+  config.action_controller.asset_host = "http://shop-annarbortees.s3.amazonaws.com/spree"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
