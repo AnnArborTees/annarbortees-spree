9b82162de97dc6a33d7f130ab54d0717b4a5dfdd
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-08-29 17:31:34 -0400

    fixed symbol in migration

--- db/migrate/20140828200444_add_layout_to_products.spree_annarbortees_theme.rb
+++ db/migrate/20140828200444_add_layout_to_products.spree_annarbortees_theme.rb
@@ -3,7 +3,7 @@ class AddLayoutToProducts < ActiveRecord::Migration
   def change
 
     change_table :spree_products do |t|
-      t.string :layout, default: default
+      t.string :layout, default: :default
     end
 
     add_index :spree_products, :layout
