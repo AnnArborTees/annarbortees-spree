bde7a68c53faa68176aa149026eaa80b5e969d42
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-04 14:44:07 -0400

    using server IP, eventually need to use AWS integration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ set :branch, '2-2-stable'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'wip.annarbortees.com', user: 'ubuntu', roles: %w{web app}#, my_property: :my_value
+server '54.221.198.113', user: 'ubuntu', roles: %w{web app}#, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
