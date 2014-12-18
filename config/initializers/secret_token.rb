7f7dda78d68773550ee75b076d0f450bb3e3ebbe
Resonious <metreckk@gmail.com>
2014-12-18 14:19:54 -0500

    Put secret_key_base into application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ShopAnnarborteesCom::Application.config.secret_key_base = 'e6f0bb07a86c0d24d30d7222b5f5899594ada634c7813c5458b4cf380dac271d243057027176bbca14f78a23a296ce22d5ba2b1e1b912ae7f6748e9c3b5e4bcf'
+ShopAnnarborteesCom::Application.config.secret_key_base = Figaro.env['secret_key_base']
