116fd4a77c7ea316cebc4a45728929c41f49bcf0
Resonious <metreckk@gmail.com>
2015-02-04 09:51:43 -0500

    catch nomethoderror on order cancellation

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -9,6 +9,15 @@ Spree::Admin::OrdersController.class_eval do
     rescue Spree::Core::GatewayError => error
       flash[:error] = "Gateway error: #{error.message}"
       cancelled = false
+
+    rescue NoMethodError => error
+      if error.message.include? 'PayPal'
+        flash[:error] = "Order is cancelled, but PayPal payments must be cancelled manually."
+        cancelled = true
+      else
+        flash[:error] = "The payment gateway in this order doesn't seem to support cancelation."
+        cancelled = false
+      end
     end
 
     redirect_to :back, just_cancelled_order: cancelled
