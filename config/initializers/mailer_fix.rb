fecdcfc32b3aff6063f0591fdddff37302f2a65a
Resonious <metreckk@gmail.com>
2015-04-10 16:17:37 -0400

    actually fix the problem

--- config/initializers/mailer_fix.rb
+++ config/initializers/mailer_fix.rb
@@ -1,16 +1,22 @@
 # Rubinius calls Enumerator#sort! within Enumerator#sort_by,
-# and Mail::PartsList calls sort_by within sort!... See the
+# # and Mail::PartsList calls sort_by within sort!... See the
 # problem?
 
-Mail::PartsList.class_eval do
-  def sort!(order = nil)
-    return super if order.nil?
-
-    i = 0
-    self.sort_by do |a|
-      [get_order_value(a, order), i += 1]
+if RUBY_ENGINE == 'rbx'
+  Mail::PartsList.class_eval do
+    def map!(&block)
+      Mail::PartsList.new(collect(&block))
     end
-    self.clear
-    sorted.each(&self.method(:<<))
+
+    def sort!(order = nil)
+      return super() if order.nil?
+
+      i = 0
+      sorted = self.sort_by do |a|
+        [get_order_value(a, order), i += 1]
+      end
+      self.clear
+      sorted.each(&self.method(:<<))
+    end
   end
 end
