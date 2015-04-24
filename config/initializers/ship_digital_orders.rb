30091ce5c16813bf660a582a32702713d2d62a55
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-04-24 15:41:01 -0400

    Only running in server mode

--- config/initializers/ship_digital_orders.rb
+++ config/initializers/ship_digital_orders.rb
@@ -1,17 +1,20 @@
 SHIP_EVERY = 1.minute
+unless defined?(Rails::Console)
+  Thread.new do
 
-Thread.new do
-  loop do
-    begin
-      Spree::Shipment.ship_digital_orders
-      ActiveRecord::Base.clear_active_connections!
-      sleep SHIP_EVERY
-    rescue Exception => e
-      Rails.logger.error "URGENT: #{e} was thrown while shipping digital downloads. Trying again in 1 minute."
-      e.backtrace.each(&Rails.logger.method(:error))
-      ActiveRecord::Base.clear_active_connections!
-      sleep SHIP_EVERY
-      Rails.logger.error "1 minute has passed, trying again..."
+    load "#{Rails.root}/app/models/spree/shipment_decorator.rb"
+    loop do
+      begin
+        Spree::Shipment.ship_digital_orders
+        ActiveRecord::Base.clear_active_connections!
+        sleep SHIP_EVERY
+      rescue Exception => e
+        Rails.logger.error "URGENT: #{e} was thrown while shipping digital downloads. Trying again in 1 minute."
+        e.backtrace.each(&Rails.logger.method(:error))
+        ActiveRecord::Base.clear_active_connections!
+        sleep SHIP_EVERY
+        Rails.logger.error "1 minute has passed, trying again..."
+      end
     end
   end
 end
