37618148658573678f2e1ecf9f41ba77e6687626
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-06-01 16:39:45 -0400

    Fixed redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,12 +5,12 @@ Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
-  config.redis = { :namespace => 'softwear-crm', :url => Figaro.env.redis_url }
+  config.redis = { :namespace => 'annarbortees-spree', :url => Figaro.env.redis_url }
 end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
   end
-  config.redis = { :namespace => 'softwear-crm', :url => Figaro.env.redis_url }
+  config.redis = { :namespace => 'annarbortees-spree', :url => Figaro.env.redis_url }
 end
