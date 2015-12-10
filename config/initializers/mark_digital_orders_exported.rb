b2ae8ef4da389cfd4044a3bce09032dcc705a406
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-12-09 22:53:30 -0500

    marking digital orders exported

---
+++ config/initializers/mark_digital_orders_exported.rb
@@ -0,0 +1,23 @@
+MARK_EXPORTED_EVERY = 1.minute
+unless defined?(Rails::Console) || Rails.env.development?
+  Thread.new do
+
+    load "#{Rails.root}/app/models/spree/shipment_decorator.rb"
+    loop do
+      begin
+        orders = Spree::Order.where.not(completed_at: nil).where(export_state: 'pending')
+        orders.each do |order|
+          shipments = order.shipments.map{|x| x.shipping_method.name }.uniq
+          order.digital_only if shipments == ['Digital Download']
+        end
+        sleep MARK_EXPORTED_EVERY
+      rescue Exception => e
+        Rails.logger.error "URGENT: #{e} was thrown while marking digital downloads. Trying again in 1 minute."
+        e.backtrace.each(&Rails.logger.method(:error))
+        ActiveRecord::Base.clear_active_connections!
+        sleep SHIP_EVERY
+        Rails.logger.error "Marking Digital Download Failures::: 1 minute has passed, trying again..."
+      end
+    end
+  end
+end
