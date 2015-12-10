57579162275f54c7084fef4f789e75400e5e170e
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-12-09 22:48:46 -0500

    fixing digital order shipment

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,13 +1,22 @@
 Spree::Shipment.class_eval do
 
   def self.ship_digital_orders
+    errors = []
     digital_shipping_methods = Spree::ShippingMethod.where(name: 'Digital Download')
     digital_shipping_methods.each do |sm|
-      shipments = Spree::Shipment.joins(:shipping_rates).where(spree_shipping_rates: {shipping_method_id: sm.id})
-      shipments.each do |s|
-        shipment = Spree::Shipment.find(s.id)
-        shipment.ship
+      begin
+        shipments = Spree::Shipment.joins(:shipping_rates).where(spree_shipping_rates: {shipping_method_id: sm.id}, state: 'ready')
+        shipments.each do |s|
+          shipment = Spree::Shipment.find(s.id)
+          shipment.ship
+        end
+      rescue Exception => e
+         errors << e
       end
+    end
+
+    unless errors.empty?
+      raise Exception, errors.join(", ")
     end
   end
 
