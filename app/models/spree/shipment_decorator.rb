1034774d20b4d9b7f06a020ad4b898916dbcb6c7
Ricky Winowiecki <slikstir@gmail.com>
2016-01-06 11:10:57 -0500

    Revising digital download shipping
    Forcing state updates if if fails in the transition call

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -4,14 +4,15 @@ Spree::Shipment.class_eval do
     errors = []
     digital_shipping_methods = Spree::ShippingMethod.where(name: 'Digital Download')
     digital_shipping_methods.each do |sm|
-      begin
-        shipments = Spree::Shipment.joins(:shipping_rates).where(spree_shipping_rates: {shipping_method_id: sm.id}, state: 'ready')
-        shipments.each do |s|
-          shipment = Spree::Shipment.find(s.id)
+      shipments = Spree::Shipment.joins(:shipping_rates).where(spree_shipping_rates: {shipping_method_id: sm.id}, state: 'ready')
+      shipments.each do |s|
+        shipment = Spree::Shipment.find(s.id) # reload because of above joins
+        begin
           shipment.ship
+        rescue StateMachine::InvalidTransition => e
+         shipment.update_attribute(:state, :shipped)
+         shipment.order.update_attribute(:shipment_state, :shipped
         end
-      rescue Exception => e
-         errors << e
       end
     end
 
