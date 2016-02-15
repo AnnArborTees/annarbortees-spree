d26e2236cfb991c7258263afaf5aaf0c77c41b70
Ricky Winowiecki <slikstir@gmail.com>
2016-02-15 16:34:26 -0500

    Rescuing from e-mail errors

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -12,6 +12,9 @@ Spree::Shipment.class_eval do
         rescue StateMachine::InvalidTransition => e
          shipment.update_attribute(:state, :shipped)
          shipment.order.update_attribute(:shipment_state, :shipped)
+        rescue ActionView::Template::Error =>  e
+         shipment.update_attribute(:state, :shipped)
+         shipment.order.update_attribute(:shipment_state, :shipped)
         end
       end
     end
