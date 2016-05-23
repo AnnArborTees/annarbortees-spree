feb22c9f644be7fa9a4f992d702f38be28b46cc9
Nigel Baillie <metreckk@gmail.com>
2016-05-23 15:25:20 -0400

    permitted attributes for images now includes option_value_id

--- config/initializers/add_store_ids_to_permitted_product_attributes.rb
+++ config/initializers/add_store_ids_to_permitted_product_attributes.rb
@@ -1,3 +1,7 @@
 unless Spree::Api::ApiHelpers.product_attributes.include?(:store_ids)
   Spree::Api::ApiHelpers.product_attributes << :store_ids
 end
+
+unless Spree::Api::ApiHelpers.image_attributes.include?(:option_value_id)
+  Spree::Api::ApiHelpers.image_attributes << :option_value_id
+end
