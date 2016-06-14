b6f63086682a5e7dc860d38b42860aa176bc325e
Nigel Baillie <metreckk@gmail.com>
2016-06-14 17:33:39 -0400

    update spree_digital

--- app/mailers/order_mailer_decorator.rb
+++ app/mailers/order_mailer_decorator.rb
@@ -2,4 +2,14 @@ Spree::OrderMailer.class_eval do
   helper 'spree/base'
 
   layout 'spree/layouts/email'
+
+  def digital_downloads_ready(order)
+    @order = order
+
+    mail(
+      to: @order.email,
+      from: Spree::Config[:mails_from],
+      subject: "Your Digital Download Links are Now Available!"
+    )
+  end
 end
