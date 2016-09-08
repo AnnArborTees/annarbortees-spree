604071ecbac65aa3e813a8d9ec9e676bed80089d
Nigel Baillie <metreckk@gmail.com>
2016-09-08 15:33:44 -0400

    allow 'layout' attribute of product to be updated over api

--- config/initializers/add_store_ids_to_permitted_product_attributes.rb
+++ config/initializers/add_store_ids_to_permitted_product_attributes.rb
@@ -6,6 +6,10 @@ unless Spree::Api::ApiHelpers.product_attributes.include?(:tax_category_id)
   Spree::Api::ApiHelpers.product_attributes << :tax_category_id
 end
 
+unless Spree::Api::ApiHelpers.product_attributes.include?(:layout)
+  Spree::Api::ApiHelpers.product_attributes << :layout
+end
+
 unless Spree::Api::ApiHelpers.image_attributes.include?(:option_value_id)
   Spree::Api::ApiHelpers.image_attributes << :option_value_id
 end
